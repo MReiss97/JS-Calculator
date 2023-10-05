17eeabea2ea8d1facc09c9d719c43255379b525c
MReiss97 <maxreiss21@gmail.com>
2023-10-05 11:47:37 -0400

    Finalized javascript

--- script.js
+++ script.js
@@ -11,38 +11,86 @@ let valMemStored = ''
 
 function numButPress(num) {
    // Check if a number has already been clicked
-   if(resultVal) {
+   if (resultVal) {
       //start a new number
-      newVal= num
+      newVal = num
       //reset to create a new result
       resultVal = ''
    } else {
       // used to block multiple decimals
       if (num === '.') {
-         if (decimalClicked != true){
+         if (decimalClicked != true) {
             //Take the current value of newVal
             // and add the character pressed
             newVal += num
             //Sets decimal check var to true, wont allow more
             decimalClicked = true
-         
+
          }
       } else {
          newVal += num
          console.log(newVal)
       }
    }
-//update the display
-document.getElementById('entry').value = newVal;
+   //update the display
+   document.getElementById('entry').value = newVal;
 }
 
 function mathButPress(operator) {
-
-
+   // Check if there was a previous calculation 
+   // by seeing if resultVal has a value
+   // if result doesn't have a value then store
+   // the current val as a previous for the
+   // calculation
+   if (!resultVal) {
+      preVal = newVal
+   } else {
+      //If there is a current result store that as
+      // the previous value entered
+      preVal = resultVal
+   }
+   //Restart creation of new number
+   newVal = ''
+   // Reset decimalClicked
+   decimalClicked = false
+   // Store operator clicked
+   mathOperator = operator
+   // Prepare entry for receiving new numbers
+   resultVal = ''
+   document.getElementById('entry').value = ''
 }
 
 function equalButPress() {
+   // Reset decimalClicked
+   decimalClicked = false
+   //Convert string numbers to floats
+   preVal = parseFloat(preVal)
+   newVal = parseFloat(newVal)
+   // Perform calculations based on the operator
+   switch(mathOperator){
+      case '+': 
+         resultVal = preVal + newVal
+         break
+         case '-':
+            resultVal = preVal - newVal
+            break
+            case '*':
+            resultVal = preVal * newVal
+            break
+            case '/':
+            resultVal = preVal / newVal
+            break
+            //If equals is hit without an operator
+            // leave everything as is
+            default:
+               preVal = newVal
+   }
+   // Store the current value as the previous so that
+   // I can except to next value in the calculation
 
+   preVal = newVal
+
+   document.getElementById('entry').value = resultVal
 
 }
 
